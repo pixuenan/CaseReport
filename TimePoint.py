f1c1c50bcc34cd94e5dc595768124521ea46def6
pishijiazu <ybhzyz@gmail.com>
2017-01-05 13:18:38 +0800

    Patient age detection finished.

--- TimePoint.py
+++ TimePoint.py
@@ -28,9 +28,17 @@ class TimePoint:
         return score
 
     def detect_age(self, utterance):
-        pattern = re.compile(r"([^\s]+?)(\s|-)(year|month|week|day)(s\s|-)(old)\s(male|female|man|woman|girl|boy|lady)")
+        """ Detect age information in the phrase, if detected, add dictionary {"Age": age} to the phrase mapping result
+        """
+        text = utterance[0]["Utterance text"]
+        pattern = re.compile(r"([^\s]+?)(\s|-)(year|month|week|day)(s\s|-)(old)")
         result = pattern.search(text)
-        age = ''.join(result.group(1, 2, 3, 4, 5))
+        if result:
+            age = ''.join(result.group(1, 2, 3, 4, 5))
+            age_dict = dict()
+            age_dict['Age'] = age
+            utterance[1]['mapping'].insert(0, age_dict)
+            print utterance[1]
         return utterance
 
     def detect_time_point(self, utterance):
@@ -39,13 +47,17 @@ class TimePoint:
 
     def time_point_extraction(self, matched_utterances):
         """ Extract time point terms"""
+        age_detected_flag = False
         for idx, utterance in enumerate(matched_utterances):
             print utterance
-            semantic_types = self.collect_needed_semantic_types(utterance)
-            if semantic_types:
-                matched_utterances[idx] = self.detect_age(utterance)
-            else:
-                matched_utterances[idx] = self.detect_time_point(utterance)
+            if not age_detected_flag:
+                semantic_types = self.collect_needed_semantic_types(utterance)
+                if semantic_types:
+                    matched_utterances[idx] = self.detect_age(utterance)
+                    age_detected_flag = True
+                    continue
+            # detect time point only when in the text there is disease or phenotype terms
+            # matched_utterances[idx] = self.detect_time_point(utterance)
         return matched_utterances
 
 if __name__ == "__main__":
@@ -53,7 +65,7 @@ if __name__ == "__main__":
     print "test"
     # print test.check_semantic_type("[inch, phsu]")
     # process the case report to group the sentence, phrase and mapping result together
-    processed_case = test.process("C:\\Users\\pix1\\PycharmProjects\\CaseReport\\testcases\\fullcase1result.txt")
+    processed_case = test.process("C:\\Users\\pix1\\PycharmProjects\\CaseReport\\testcases\\fullcase2result.txt")
     # turn the processed case from list into dictionary, and only keep the needed field for every mapping result
     pruned_case = test.prune(processed_case)
     matched_case = test.match(pruned_case)
